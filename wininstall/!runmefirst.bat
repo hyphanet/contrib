6ddb86f88c921e2bde617a7df2fa9d7530c8d4ba
codeshark <codeshark@unknown>
2001-01-22 21:16:17 +0000

    stupid batch added
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/contrib@59 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ wininstall/!runmefirst.bat
@@ -0,0 +1,5 @@
+@echo off
+copy Freenet\fserve.exe Freenet\frequest.exe
+copy Freenet\fserve.exe Freenet\finsert.exe
+copy Freenet\fserve.exe Freenet\FProxy.exe
+copy Freenet\fserve.exe Freenet\cfgnode.exe
