82a8893683fb7cc9a177b4e0015f3e13feab2dbd
Mark J Roberts <mjr@users.sourceforge.net>
2001-05-09 02:49:25 +0000

    sdf
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/contrib@242 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ mjr/libfcp/spammer.c
@@ -0,0 +1,21 @@
+#include <fcp.h>
+#include <sys/stat.h>
+
+int
+main (int argc, char **argv)
+{
+    int i = atoi(argv[3]), end = i + atoi(argv[4]);
+    char uri[128];
+    FILE *data = fopen(argv[2], "r");
+    struct stat s;
+    stat(argv[2], &s);
+    while (i++ < end) {
+        sprintf(uri, "%s%d", argv[1], i);
+	rewind(data);
+	printf("Inserting %s.\n", uri);
+	fflush(stdout);
+	fcp_insert_raw(data, uri, s.st_size, FCP_DATA, 10);
+    }
+    return 0;
+}
+
