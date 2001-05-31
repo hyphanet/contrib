a7764974d6d7b9d7fb1cadd9eea14a227ed54514
stripwax <stripwax@unknown>
2001-05-31 01:36:24 +0000

    Moved some #defines to rsrc.h ; added some function prototypes
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/contrib@335 67a373e5-eb02-0410-a15c-ee090a768436

--- wininstall/Sources/freenet.exe/freenet_tray.h
+++ wininstall/Sources/freenet.exe/freenet_tray.h
@@ -3,11 +3,6 @@
 
 #define WM_SHELLNOTIFY WM_USER+5 
 #define IDI_TRAY 0 
-#define IDM_STARTSTOP 1001
-#define IDM_GATEWAY 1004 
-#define IDM_CONFIGURE 1005
-#define IDM_SHOWLOG 1006
-#define IDM_EXIT 1010
 
 #define GATEWLEN 32
 
@@ -17,6 +12,11 @@ LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 /* startup code: */
 bool OnlyOneInstance(void);
 void Initialise(void);
+void GetAppDirectory(char * szbuffer);
+
+/* Reloading settings from flaunch.ini, etc.  */
+void ReloadSettings(void);
+
 
 /* message-pump (i.e. WindowProc)-level functions to start and stop freenet node */
 /* note - ASYNCHRONOUS - fire-and-forget messages - for true multithreaded interface */
@@ -36,4 +36,4 @@ void GetFirstToken(LPSTR szCurrentPointer, LPSTR *pszEndPointer);
 LPSTR GetNextToken(LPSTR szCurrentPointer, const LPSTR szEndPointer);
 void GetToken(LPSTR szCurrentPointer);
 LPSTR SkipSpace(LPSTR szString);
- 
+
