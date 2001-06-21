be3d628ddf8614a6cae381a97cc6e7feb9b2fcd9
stripwax <stripwax@unknown>
2001-06-21 01:24:25 +0000

    Additional states in support of additional status monitoring
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/contrib@439 67a373e5-eb02-0410-a15c-ee090a768436

--- wininstall/Sources/freenet.exe/types.h
+++ wininstall/Sources/freenet.exe/types.h
@@ -14,13 +14,17 @@ typedef enum _tagbool{false=0,true} bool;
 
 typedef void(__stdcall* LPCONFIGPROC)(void*);
 
+/* internal state enum.  Note - it may be the case that some states are not currently used in the code */
 typedef enum _tagfreenetmode
 {
-	FREENET_STOPPED	= 0,
-	FREENET_RUNNING,
-	FREENET_RESTARTING,
-	FREENET_STOPPING,
-	FREENET_CANNOT_START
+	FREENET_STOPPED	= 0,				/* indicates node is not running - has been stopped or has never been started */
+	FREENET_RUNNING,					/* indicates node is running */
+	FREENET_RUNNING_NO_GATEWAY,			/* indicates node is running but FProxy is not */
+	FREENET_RUNNING_NO_INTERNET,		/* indicates node is running but internet connection is 'down' in some way */
+	FREENET_RESTARTING,					/* indicates node is stopping but that immediately after stoppage will begin starting again */
+	FREENET_STOPPING,					/* indicates node is stopping and that after stoppage will end up in the FREENET_STOPPED state */
+	FREENET_CANNOT_START,				/* indicates node is not running and that a recent attempt to start (or restart) the node failed */
+	FREENET_NOT_RUNNING_NO_INTERNET		/* indicates node is not running and that the internet connection is 'down' in some way */
 } FREENET_MODE;
 
 
