f274f0034b3cfb08847ecec922529b3cf7dcbd30
Sebastian Spaeth <spaetz@users.sourceforge.net>
2001-09-13 23:01:47 +0000

    Adding GetSeed Project
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/contrib@573 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ wininstall/Sources/GetSeed/GetSeedDlg.cpp
@@ -0,0 +1,137 @@
+// GetSeedDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "afxinet.h"
+#include "Wininet.h"
+#include "GetSeed.h"
+#include "GetSeedDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CGetSeedDlg dialog
+
+CGetSeedDlg::CGetSeedDlg(CWnd* pParent /*=NULL*/)
+	: CDialog(CGetSeedDlg::IDD, pParent)
+{
+	CString str = _pgmptr;
+	str = str.Left(str.ReverseFind('\\')+1) + DEFSEEDFILE; //Take the executable Program path
+		//{{AFX_DATA_INIT(CGetSeedDlg)
+	m_seedURL = _T(DEFSEEDURL);
+	m_seedfile = _T(str);
+	//}}AFX_DATA_INIT
+	// Note that LoadIcon does not require a subsequent DestroyIcon in Win32
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CGetSeedDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CGetSeedDlg)
+	DDX_Text(pDX, IDC_SEEDURL, m_seedURL);
+	DDX_Text(pDX, IDC_SEEDFILE, m_seedfile);
+	//}}AFX_DATA_MAP
+}
+
+BEGIN_MESSAGE_MAP(CGetSeedDlg, CDialog)
+	//{{AFX_MSG_MAP(CGetSeedDlg)
+	ON_BN_CLICKED(IDC_GETSEED, OnGetseed)
+	ON_BN_CLICKED(IDC_GETLOCALSEED, OnGetlocalseed)
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CGetSeedDlg message handlers
+
+BOOL CGetSeedDlg::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+
+	// Set the icon for this dialog.  The framework does this automatically
+	//  when the application's main window is not a dialog
+	SetIcon(m_hIcon, TRUE);			// Set big icon
+	SetIcon(m_hIcon, FALSE);		// Set small icon
+	
+	// TODO: Add extra initialization here
+	
+	return TRUE;  // return TRUE  unless you set the focus to a control
+}
+
+BOOL CGetSeedDlg::OnGetseed()
+//returns TRUE on success
+{
+	HINTERNET hInternet, hhttpFile;
+	HANDLE hfile;
+	CString str;
+	char buf[512];
+	BOOL success = 1, doDialUp = 0;
+	DWORD filesize = 0;
+	DWORD size_fsize = 4; //size of filesize buffer in bytes
+	DWORD dwFlags = INTERNET_STATE_DISCONNECTED; //check for existing connection flags
+	DWORD read;
+	DWORD written;
+
+	//DWORD InternetHangUp(DWORD dwConnection,DWORD dwReserved);
+	
+	//vvvv FALSE if there is no Internet connection
+	if (!InternetGetConnectedState(&dwFlags,0))
+	{
+		str.LoadString(IDS_NOTCONNECTED);
+		if (MessageBox(str,NULL,MB_OKCANCEL) == IDCANCEL)
+		{ //abort the download and quit the function
+			return FALSE;
+		}
+		doDialUp = TRUE;
+	}
+
+	hInternet = InternetOpen(AfxGetApp()->m_pszAppName, INTERNET_OPEN_TYPE_PRECONFIG , NULL, NULL, NULL /*dwFlags*/); // returns NULL on failure
+	hhttpFile = InternetOpenUrl(hInternet, m_seedURL, NULL, NULL, INTERNET_FLAG_NO_COOKIES|INTERNET_FLAG_RESYNCHRONIZE|INTERNET_FLAG_NO_UI, NULL); //return FAIL on error
+	//Get the filesize via HttpQueryInfo if we need a progress meter at one time
+	//HttpQueryInfo(hhttpFile,HTTP_QUERY_CONTENT_LENGTH|HTTP_QUERY_FLAG_NUMBER,&filesize,&size_fsize,NULL);
+	//MessageBox(itoa(filesize,(char*)&buf,10));
+	success = ((hfile = CreateFile(m_seedfile, GENERIC_WRITE, 0, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0)) != INVALID_HANDLE_VALUE) && success;
+	do 
+	{
+		success = InternetReadFile(hhttpFile, buf, 512, &read) && success;
+		success = WriteFile(hfile, buf, read, &written, 0) && success;
+		success = (written == read) && success;
+	} while (written && success);
+
+	// Cleanup now
+	InternetCloseHandle(hhttpFile);	// close the remote http file
+	InternetCloseHandle(hInternet);	// close the Internet handle
+	if (doDialUp) {InternetAutodialHangup(0);}
+	CloseHandle(hfile);
+	if (success)
+		EndDialog(1);
+	else
+	{
+		DeleteFile(m_seedfile);
+		str.LoadString(IDS_ERR_GETSEED);
+		MessageBox(str,NULL,MB_OK|MB_ICONERROR);
+	}
+		
+	return success;
+}
+
+BOOL CGetSeedDlg::OnGetlocalseed() 
+{	
+	BOOL success;
+	CString str;
+
+	CFileDialog m_ldFile(TRUE, NULL, NULL,OFN_FILEMUSTEXIST|OFN_HIDEREADONLY,"Freenet reference file (*.ref)|*.ref|All Files (*.*)|*.*||",GetParent());
+
+	m_ldFile.DoModal();
+	success = CopyFile(m_ldFile.GetFileName(),m_seedfile,FALSE);	// return TRUE on success
+	if (!success) 
+	{
+		str.LoadString(IDS_ERR_GETSEED);
+		MessageBox(str,NULL,MB_OK|MB_ICONERROR);
+	}
+	return success;
+}
---
+++ wininstall/Sources/GetSeed/resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by GetSeed.rc
+//
+#define IDD_GetSeed_DIALOG           102
+#define IDP_SOCKETS_INIT_FAILED         103
+#define IDS_NOTCONNECTED                104
+#define IDS_ERR_GETSEED                 105
+#define IDR_MAINFRAME                   128
+#define IDC_GETSEED                     1000
+#define IDC_SEEDURL                     1001
+#define IDC_SEEDFILE                    1002
+#define IDC_GETLOCALSEED                1004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
