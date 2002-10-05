5a04a73dbd08695d730f538baee919462f2c7d07
Jay Oliveri <ilnero@gmx.net>
2002-10-05 02:49:21 +0000

    incremental
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/contrib@1762 67a373e5-eb02-0410-a15c-ee090a768436

--- fcptools/fcpget/fcpget.c
+++ fcptools/fcpget/fcpget.c
@@ -7,15 +7,13 @@
 
 #include <sys/stat.h>
 
-#include "ezFCPlib.h"
+#include <fcntl.h>
+#include <stdio.h>
 
 #define _GNU_SOURCE
 #include "getopt.h"
 
-#include <fcntl.h>
-#include <stdio.h>
-
-int fcpLogCallback(int level, char *buf);
+#include "ezFCPlib.h"
 
 
 /*
@@ -28,129 +26,117 @@ static void usage(char *msg);
 	 Strings/Arrays that have default values if not set explicitly from the
 	 command line should be initialized to "", a zero-length string.
 */
-char  keyUri[L_URI];
-char  keyFile[L_FILENAME] = "";
-char  metaFile[L_FILENAME] = "";
+char *keyUri;
+char *keyFile;
+char *metaFile;
 
-char  nodeAddr[L_HOST] = EZFCP_DEFAULT_HOST;
-int   nodePort = EZFCP_DEFAULT_PORT;
-int   htlVal = EZFCP_DEFAULT_HTL;
-int   regress = EZFCP_DEFAULT_REGRESS;
-int   rawMode = EZFCP_DEFAULT_RAWMODE;
-
-int   verbosity = FCP_LOG_NORMAL;
 
 int main(int argc, char* argv[])
 {
 	HFCP *hfcp;
-    char buf[1024];
-    int count;
-    int fd;
-
-    int numtimes = 3;
-    int i;
-
-    /* go thru command line args */
-    parse_args(argc, argv);
-
-    /* try and fire up FCP library */
-    _fcpLog(FCP_LOG_VERBOSE, "Attempting secret handshake with %s:%d", nodeAddr, nodePort);
-
-    if (fcpStartup(nodeAddr, nodePort, htlVal, rawMode, 0) != 0) {
-	_fcpLog(FCP_LOG_CRITICAL, "Failed to connect to node - aborting");
-	return 1;
-    }
-
-    _fcpLog(FCP_LOG_VERBOSE, "Successfully connected to node");
-
+	char buf[1024];
+	int count;
+	int fd;
+	
+	int numtimes = 3;
+	int i;
+	
+	/* go thru command line args */
+	parse_args(argc, argv);
+	
+	/* try and fire up FCP library */
+	if (fcpStartup()) {
+		_fcpLog(FCP_LOG_CRITICAL, "Failed to connect to node - aborting");
+		return 1;
+	}
+	
 	/* create an FCP handle */
 	hfcp = fcpCreateHandle();
-	fcpSetHtl(hfcp, htlVal);
-
+	
 	/* repeat many times - hunting mem leaks */
-    for (i = 0; i < numtimes; i++) {
+	for (i = 0; i < numtimes; i++) {
 		int res;
-
+		
 		/* try to get the key open */
 		_fcpLog(FCP_LOG_VERBOSE, "Trying to open '%s'", keyUri);
 		res = fcpOpenKey(hfcp, keyUri, (_FCP_O_READ | (hfcp->raw ? _FCP_O_RAW : 0)));
-
+		
 		if (res != 0)
 			_fcpLog(FCP_LOG_CRITICAL, "Failed to open '%s', retry %d", keyUri, i);
 		else
 			break;
 	}
-
+	
 	if (i<numtimes) {
-
+		
 		/* output key data, if any */
 		if (hfcp->keysize > 0) {
-
+			
 			/* nuke file if it exists */
-	    if (keyFile[0]) {
+			if (keyFile[0]) {
 				unlink(keyFile);
-
+				
 #ifdef WINDOWS
-		/* open a file to write the key to */
-		if ((fd = open(keyFile, _O_CREAT | _O_RDWR | _O_BINARY, _S_IREAD | _S_IWRITE)) < 0) {
-		    printf("Cannot create file '%s'\n", keyFile);
-		    return -1;
-		}
+				/* open a file to write the key to */
+				if ((fd = open(keyFile, _O_CREAT | _O_RDWR | _O_BINARY, _S_IREAD | _S_IWRITE)) < 0) {
+					printf("Cannot create file '%s'\n", keyFile);
+					return -1;
+				}
 #else
-		/* open a file to write the key to */
-		if ((fd = open(keyFile, O_CREAT| O_WRONLY, S_IRUSR | S_IWUSR)) < 0) {
-		    printf("Cannot create file '%s'\n", keyFile);
-		    return -1;
-		}
+				/* open a file to write the key to */
+				if ((fd = open(keyFile, O_CREAT| O_WRONLY, S_IRUSR | S_IWUSR)) < 0) {
+					printf("Cannot create file '%s'\n", keyFile);
+					return -1;
+				}
 #endif
-	    } /* if (keyFile[0]) */
-	    else
-		fd = 1; /* I believe fd==1 is STDOUT */
-
-	    /* suck all of key's data into this file */
-	    while ((count = fcpReadKey(hfcp, buf, 1024)) > 0)
-		write(fd, buf, count);
-
-	    if (fd != 1) close(fd);
+			} /* if (keyFile[0]) */
+			else
+				fd = 1; /* I believe fd==1 is STDOUT */
+			
+			/* suck all of key's data into this file */
+			while ((count = fcpReadKey(hfcp, buf, 1024)) > 0)
+				write(fd, buf, count);
+			
+			if (fd != 1) close(fd);
 		} /* if (hfcp->keysize > 0) */
-
-	if (hfcp->rawMetadata) {
-
-	    if (metaFile[0]) {
+		
+		if (hfcp->rawMetadata) {
+			
+			if (metaFile[0]) {
 				if ((metaFile[0]== '-') && (metaFile[1]=='\0'))
 					fd = 1;
-
+				
 				else {
 					/* nuke file if it exists */
-			unlink(metaFile);
+					unlink(metaFile);
 #ifdef WINDOWS
 					/* open a file to write the key to */
-				if ((fd = open(metaFile, _O_CREAT | _O_RDWR | _O_BINARY, _S_IREAD | _S_IWRITE)) < 0) {
-					    printf("Cannot create file '%s'\n", metaFile);
-					    return -1;
+					if ((fd = open(metaFile, _O_CREAT | _O_RDWR | _O_BINARY, _S_IREAD | _S_IWRITE)) < 0) {
+						printf("Cannot create file '%s'\n", metaFile);
+						return -1;
 					}
 #else
 					/* open a file to write the key to */
 					if ((fd = open(metaFile, O_CREAT| O_WRONLY, S_IREAD | S_IWRITE)) < 0) {
-							printf("Cannot create file '%s'\n", metaFile);
-					     return -1;
+						printf("Cannot create file '%s'\n", metaFile);
+						return -1;
 					}
 #endif
 				}
 				/* suck all of key's data into this file */
 				write(fd, hfcp->rawMetadata, strlen(hfcp->rawMetadata));
-
+				
 				if(fd != 1) close(fd);
 			} /* if (metaFile[0]) */
-	} /* if (hfcp->rawMetadata) */
-
-	/* all done */
-	fcpCloseKey(hfcp);
-	fcpDestroyHandle(hfcp);
-
-    } /* if (i<numtimes) */
-
-    return 0;
+		} /* if (hfcp->rawMetadata) */
+		
+			/* all done */
+		fcpCloseKey(hfcp);
+		fcpDestroyHandle(hfcp);
+		
+	} /* if (i<numtimes) */
+	
+	return 0;
 }
 
 /* IMPORTANT
@@ -180,91 +166,88 @@ static void parse_args(int argc, char *argv[])
     switch (c) {
 
     case 'n':
-      strncpy( nodeAddr, optarg, L_HOST );
+      fcpSetHost(optarg);
       break;
 
     case 'p':
       i = atoi( optarg );
-      if (i > 0) nodePort = i;
+      if (i > 0) _fcpPort = i;
       break;
 
     case 'l':
       i = atoi( optarg );
-      if (i > 0) htlVal = i;
+      if (i > 0) _fcpHtl = i;
       break;
 
     case 'e':
       i = atoi( optarg );
-      if (i > 0) regress = i;
+      if (i > 0) _fcpRegress = i;
       break;
 
     case 'r':
-      rawMode = 1;
+      _fcpRawmode = 1;
       break;
 
     case 'm':
-      strncpy( metaFile, optarg, L_FILENAME );
+			metaFile = (char *) malloc(strlen(optarg) + 1);
+			
+      strcpy(metaFile, optarg);;
       break;
 
     case 'v':
       i = atoi( optarg );
-      if ((i >= 0) && (i <= 4)) verbosity = i;
+      if ((i >= 0) && (i <= 4)) _fcpVerbosity = i;
       break;
-
+			
     case 'V':
       printf( "FCPtools Version %s\n", VERSION );
       exit(0);
-
+			
     case 'h':
       usage(0);
       break;
     }
   }
-
-  if (optind < argc) strncpy(keyUri, argv[optind++], L_URI);
+	
+  if (optind < argc) {
+		metaFile = (char *) malloc(strlen(argv[optind]) + 1);
+		strcpy(keyUri, argv[optind++]);
+	}
   else usage("You must specify a key");
-
-  /* If there's another parameter, it's the FILE to store the results in.
-     Default value is "" if not passed */
-
-  if (optind < argc)
-    strncpy(keyFile, argv[optind++], L_FILENAME);
+	
+  /* If there's another parameter, it's the FILE to store the results (optional) */
+  if (optind < argc) {
+		keyFile = (char *) malloc(strlen(argv[optind]) + 1);
+    strcpy(keyFile, argv[optind++]);
+	}
 }
 
 
 static void usage(char *s)
 {
-    if (s) printf("Error: %s\n", s);
-
-    printf("FCPtools; Freenet Client Protocol Tools\n");
-    printf("Copyright (c) 2001 by David McNab\n\n");
-
-    printf("Usage: fcpget [OPTIONS] key [file]\n\n");
-
-    printf("Options:\n\n");
-    printf("  -n, --address host   Freenet node address (default \"%s\")\n", EZFCP_DEFAULT_HOST);
-    printf("  -p, --port num       Freenet node port (default %d)\n", EZFCP_DEFAULT_PORT);
-    printf("  -l, --htl num	       Hops to live (default %d)\n", EZFCP_DEFAULT_HTL);
-		printf("  -e, --regress num    Number of days to regress (default %d)\n", EZFCP_DEFAULT_REGRESS);
-    printf("  -r, --raw            Raw mode - don't follow redirects\n\n");
-
-    printf("  -m, --metadata file  Write key's metadata to file (default \"stdout\")\n");
-    printf("  -v, --verbosity num  Verbosity of log messages (default 2)\n");
-    printf("                       0=silent, 1=critical, 2=normal, 3=verbose, 4=debug\n\n");
-
-    printf("  -V, --version	       Output version information and exit\n");
-    printf("  -h, --help           Display this help and exit\n\n");
-
-    printf("  key                  Freenet key (freenet:KSK@gpl.txt)\n");
-    printf("  file                 Write key's data to file (default \"stdout\")\n\n");
-
-    exit(0);
-}
-
-int fcpLogCallback(int level, char *buf)
-{
-  if (level <= verbosity)
-    printf("%s\n", buf);
-
-  return 0;
+	if (s) printf("Error: %s\n", s);
+	
+	printf("FCPtools; Freenet Client Protocol Tools\n");
+	printf("Copyright (c) 2001 by David McNab\n\n");
+	
+	printf("Usage: fcpget [OPTIONS] key [file]\n\n");
+	
+	printf("Options:\n\n");
+	printf("  -n, --address host   Freenet node address (default \"%s\")\n", EZFCP_DEFAULT_HOST);
+	printf("  -p, --port num       Freenet node port (default %d)\n", EZFCP_DEFAULT_PORT);
+	printf("  -l, --htl num	       Hops to live (default %d)\n", EZFCP_DEFAULT_HTL);
+	printf("  -e, --regress num    Number of days to regress (default %d)\n", EZFCP_DEFAULT_REGRESS);
+	printf("  -r, --raw            Raw mode - don't follow redirects\n\n");
+	
+	printf("  -m, --metadata file  Write key's metadata to file (default \"stdout\")\n");
+	printf("  -v, --verbosity num  Verbosity of log messages (default 2)\n");
+	printf("                       0=silent, 1=critical, 2=normal, 3=verbose, 4=debug\n\n");
+	
+	printf("  -V, --version	       Output version information and exit\n");
+	printf("  -h, --help           Display this help and exit\n\n");
+	
+	printf("  key                  Freenet key (freenet:KSK@gpl.txt)\n");
+	printf("  file                 Write key's data to file (default \"stdout\")\n\n");
+	
+	exit(0);
 }
--- fcptools/fcpput/fcpput.c
+++ fcptools/fcpput/fcpput.c
@@ -1,240 +1,138 @@
 /*
-	fcpget.c - simple command line client that uses FCP
-	CopyLeft () 2001 by David McNab
+  This code is part of FCPTools - an FCP-based client library for Freenet
+	
+  Designed and implemented by David McNab <david@rebirthing.co.nz>
+  CopyLeft (c) 2001 by David McNab
+	
+	Currently maintained by Jay Oliveri <ilnero@gmx.net>
+	
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
+
 
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#include "ezFCPlib.h"
+#include <stdio.h>
 
 #define _GNU_SOURCE
 #include "getopt.h"
 
-#ifndef WINDOWS
-#include <unistd.h>
-#endif
+#include "ezFCPlib.h"
 
-#include <stdio.h>
 
-#define FCPPUT_ATTEMPTS 3
+void  parse_args(int argc, char *argv[]);
+char *stdin_keydata(void);
+void  usage(char *);
 
-extern int fcpSplitChunkSize;
+extern char *crTmpFilename(void);
 
-static void parse_args(int argc, char *argv[]);
-static void usage(char *);
-static char *strsav(char *old, char *text_to_append);
-static char *bufsav(char *old, int old_len, char *buf_to_append, int add_len);
-static int parse_num(char *s);
-
-/* Configurable command-line parameters
-   Strings/Arrays that have default values if not set explicitly from the
-   command line should be initialized to "", a zero-length string.
-*/
-char  keyUri[L_URI];
-char  keyFile[L_FILENAME] = "";
-char  metaFile[L_FILENAME] = "";
-char *metaData = 0;
- 
-char  nodeAddr[L_HOST] = EZFCP_DEFAULT_HOST;
-int   nodePort = EZFCP_DEFAULT_PORT;
-int   htlVal = EZFCP_DEFAULT_HTL;
-int   regress = EZFCP_DEFAULT_REGRESS;
-int   rawMode = EZFCP_DEFAULT_RAWMODE;
-int   quiet = 0;
-int   attempts = FCPPUT_ATTEMPTS;
-int   splitThreads = FCP_MAX_SPLIT_THREADS;
-
-int   verbosity = FCP_LOG_NORMAL;
+char  *keyuri = 0;
+char  *keyfile = 0;
+char  *metafile = 0;
 
 
 int main(int argc, char* argv[])
 {
-    HFCP *hfcp;
-    char *keyData = 0;
-    char keyBuf[4096];
-    int curlen, newlen;
-    int insertError;
-    int fd;
+	hFCP *hfcp;
+	int rc;
 
-		struct stat fileStat;
+	char buf[L_FD_BLOCKSIZE];
+	int fd
 
-    parse_args(argc, argv);
+	struct stat put_file;
 
-    // try and fire up FCP library
-    if (fcpStartup(nodeAddr, nodePort, htlVal, rawMode, splitThreads) != 0)
-        return 1;
+	/* Must occur before any FCP related calls to set parameters prior */
+	parse_args(argc, argv);
+	
+	if (fcpStartup()) {
+		printf("Call to fcpStartup() failed\n");
+		return 1;
+	}
 
-    // create an FCP handle
-    hfcp = fcpCreateHandle();
-    fcpSetHtl(hfcp, htlVal);
+	/* If file is not specified, read key data from stdin */
+	if (!keyfile) keyfile = stdin_keydata();
 
-    // get hold of some metadata if needed
-    if (metaFile[0])
-    {
-        char meta_stdin = !strcmp(metaFile, "stdin");
-        FILE *fp_meta = meta_stdin
-            ? stdin
-            : fopen(metaFile, "r");
-        char metaLine[256];
+  if (stat(keyfile, &put_file)) return -1;
+	hfcp = _fcpCreateHFCP();
 
-        if (fp_meta == 0)
-        {
-            printf("fcpput: failed to open metadata file '%s'\n", metaFile);
-            exit(1);
-        }
+	if (!(rc = fcpOpenKeyWrite(hfcp, keyuri)))	{
+		_fcpLog(FCP_LOG_CRITICAL, "fcpput: cannot open key writing");
+		return 1;
+	}
 
-        if (meta_stdin)
-        {
-            // read metadata from stdin)
-            metaData = strsav(0, "");
+	/* Now write the key to Freenet */
+	if ((fd = open(filename, O_RDONLY, S_IRUSR)) == -1) return 1;
 
-            if (!quiet)
-                printf("Enter metadata line by line, enter a line '.' to finish:\n");
-            while (fgets(metaLine, sizeof(metaLine), fp_meta) != 0)
-                if (metaLine[0] == '.')
-                    break;
-                else
-                {
-                    metaData = strsav(metaData, metaLine);
-                    metaData = strsav(metaData, "\n");
-                }
-        }
-        else
-        {
-            while (fgets(metaLine, sizeof(metaLine), fp_meta) != 0)
-            {
-                metaData = strsav(metaData, metaLine);
-                //metaData = strsav(metaData, "\n");
-            }
-        }
-    }
+	while ((rc = read(fd, buf, L_FD_BLOCKSIZE)) > 0)
+		fcpWriteKey(hfcp, buf, 8192));
 
-    // read key data from stdin if required
-    if (keyFile == 0)
-    {
-        keyData = 0;
-        fd = 0;
+	/* Clean it up for demo purposes */
+	fcpCloseKey(hfcp);
+	fcpTerminate(hfcp);
+	_fcpDestroyHFCP(hfcp);
 
-        // read key from stdin
-        if (!quiet)
-            printf("Enter key data line by line, prees Control-D to finish:\n");
-
-        curlen = 0;
-
-        while ((newlen = read(fd, keyBuf, 4096)) > 0)
-        {
-            keyData = bufsav(keyData, curlen, keyBuf, newlen);
-            curlen += newlen;
-        }
-
-        // ok - got metadata and file in mem, insert it
-        insertError = fcpPutKeyFromMem(hfcp, keyUri, keyData, metaData, curlen);
-        free(keyData);
-    }
-    else
-    {
-		int fileSize;
-		char chk[L_URI];
-		char metaRedir[1024];
-
-		if (stat(keyFile, &fileStat) < 0)
-		{
-			_fcpLog(FCP_LOG_CRITICAL, "fcpput: cannot stat '%s'", keyFile);
-			return -1;
-		}
-
-		fileSize = fileStat.st_size;
-
-		if (fileSize < L_KSK || strstr(keyUri, "CHK@"))
-		{
-			int i = 1;
-
-			do
-				_fcpLog(FCP_LOG_VERBOSE,
-					"Key is small or chk, inserting directly, attempt %d/%d",
-					i, attempts);
-			while ((insertError = fcpPutKeyFromFile(hfcp, keyUri, keyFile, metaData)) != 0 && i++ <= attempts);
-		}
-		else
-		{
-			int i = 0;
-
-			_fcpLog(FCP_LOG_VERBOSE, "Key too big, inserting as CHK");
-
-			// gotta insert as CHK, then insert a redirect to it
-			while ((insertError = fcpPutKeyFromFile(hfcp, "CHK@", keyFile, metaData)) != 0 && i++ < attempts)
-			{
-				printf("Insert attempt %d/%d failed\n", i, attempts);
-				return -1;
-			}
-
-			// uplift the CHK URI, generate metadata for a redirect
-			strcpy(chk, hfcp->created_uri);
-			_fcpLog(FCP_LOG_VERBOSE, "Inserted as %s", chk);
-
-			sprintf(metaRedir,
-					"Version\nRevision=1\nEndPart\nDocument\nRedirect.Target=%s\nEnd\n",
-					chk);
-
-			// insert the redirect
-			i = 0;
-			while ((insertError = fcpPutKeyFromMem(hfcp, keyUri, 0, metaRedir, 0)) != 0 && i++ < attempts)
-			{
-				printf("Redirect insert attempt %d/%d failed\n", i, attempts);
-				return -1;
-			}
-			_fcpLog(FCP_LOG_VERBOSE, "Redirect inserted successfully");
-		}
-
-    }
-
-    // clean up
-    if (metaData != 0)
-        free(metaData);
-    if (fd != 0)
-        close(fd);
-
-    // if insert worked, extract the actual URI
-    if (insertError == 0)
-        puts(hfcp->created_uri);
-	else
-		printf("Failed to insert, sorry\n");
-
-    // all done
-    fcpDestroyHandle(hfcp);
-    return insertError;
+	return 0;
 }
 
-int fcpLogCallback(int level, char *buf)
+
+char *stdin_keydata(void)
 {
-    if (level <= verbosity)
-        puts(buf);
-    return 0;
+	char *filename = 0;
+	int fd_tmpfile;
+	int i;
+
+	char buf[L_FD_BLOCKSIZE + 1];
+
+	filename = crTmpFilename();
+	if ((fd_tmpfile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR)) == -1) return 0;
+
+	while ((i = read(STDIN_FILENO, buf, L_FD_BLOCKSIZE)) > 0)
+		write(fd_tmpfile, buf, i);
+
+	close(fd_tmpfile);
+	return filename;
 }
+
 
 /* IMPORTANT
    This function should bail if the parameters are bad in any way.  main() can
    then continue cleanly. */
 
-static void parse_args(int argc, char *argv[])
+void parse_args(int argc, char *argv[])
 {
-  static struct option long_options[] = {
+  struct option long_options[] = {
     {"address", 1, 0, 'n'},
     {"port", 1, 0, 'p'},
     {"htl", 1, 0, 'l'},
+    {"regress", 1, 0, 'e'},
     {"raw", 0, 0, 'r'},
+    {"key", 1, 0, 'k'},
     {"metadata", 1, 0, 'm'},
-		{"size", 1, 0, 's'},
-		{"quiet", 0, 0, 'q'},
-		{"attempts", 1, 0, 'a'},
-		{"threads", 1, 0, 't'},
+
     {"verbosity", 1, 0, 'v'},
+		{"attempts", 1, 0, 'a'},
+		{"size", 1, 0, 's'},
+		{"threads", 1, 0, 't'},
+
     {"version", 0, 0, 'V'},
     {"help", 0, 0, 'h'},
+
     {0, 0, 0, 0}
   };
-  static char short_options[] = "l:n:p:e:m:s:qa:t:rv:Vh";
+  char short_options[] = "n:p:l:e:rk:m:v:a:s:t:Vh";
 
   /* c is the option code; i is buffer storage for an int */
   int c, i;
@@ -243,92 +141,111 @@ static void parse_args(int argc, char *argv[])
     switch (c) {
 
     case 'n':
-      strncpy( nodeAddr, optarg, L_HOST );
+			if (_fcpHost) free(_fcpHost);
+			_fcpHost = (char *)malloc(strlen(optarg) + 1);
+
+      strcpy( _fcpHost, optarg);
       break;
 
     case 'p':
       i = atoi( optarg );
-      if (i > 0) nodePort = i;
+      if (i > 0) _fcpPort = i;
       break;
 
     case 'l':
       i = atoi( optarg );
-      if (i > 0) htlVal = i;
+      if (i >= 0) _fcpHtl = i;
       break;
+
+		case 'e':
+			i = atoi( optarg );
+			if (i > 0) _fcpRegress = i;
 
     case 'r':
-      rawMode = 1;
+      _fcpRawmode = 1;
       break;
 
-    case 'm':
-      strncpy( metaFile, optarg, L_FILENAME );
+    case 'k':
+			keyuri = (char *)malloc(strlen(optarg) + 1);
+      strcpy(keyuri, optarg);
       break;
 
-		case 's':
-			i = atoi( optarg );
-			if (i > 0) fcpSplitChunkSize = i;
-			break;
+			case 'm':
+			metafile = (char *)malloc(strlen(optarg) + 1);
+      strcpy(metafile, optarg);
+      break;
 
-		case 'q':
-			quiet = 1;
-			break;
+    case 'v':
+      i = atoi( optarg );
+      if ((i >= 0) && (i <= 4)) _fcpVerbosity = i;
+      break;
 
 		case 'a':
 			i = atoi( optarg );
-			if (i > 0) attempts = i;
+			if (i > 0) _fcpInsertAttempts = i;
 			break;
 
-		case 't':
+			/*case 's':
+			i = atoi( optarg );
+			if (i > 0) fcpSplitChunkSize = i;
+			break;*/
+
+			/*case 't':
 			i = atoi( optarg );
 			if (i > 0) splitThreads = i;
-			break;
+			break;*/
  
-    case 'v':
-      i = atoi( optarg );
-      if ((i >= 0) && (i <= 4)) verbosity = i;
-      break;
-
     case 'V':
       printf( "FCPtools Version %s\n", VERSION );
       exit(0);
 
     case 'h':
       usage(0);
-      break;
+      exit(0);
 		}
 	}
 
-  if (optind < argc) strncpy(keyUri, argv[optind++], L_URI);
-  else usage("You must specify a key");
+  if (optind < argc) {
+		keyfile = (char *)malloc(strlen(argv[optind]) + 1);
+		strcpy(keyfile, argv[optind++]);
+	}
+	else {
+		if (!keyfile) {
+			/* This means no file to insert was specified via "file" param, nor -k */
+			usage("You must specify a file parameter, or use the option \"-k\"");
+			exit(1);
+		}
+	}
 
-  /* If there's another parameter, it's the FILE to store the results in.
-     Default value is "" if not passed */
-  if (optind < argc)
-    strncpy(keyFile, argv[optind++], L_FILENAME);
+	if (!keyuri) {
+		keyuri = (char *)malloc(5);
+		strcpy(keyuri, "CHK@");
+	}
 }
 
-static void usage(char *s)
+
+void usage(char *s)
 {
 	if (s) printf("Error: %s\n", s);
 
 	printf("FCPtools; Freenet Client Protocol Tools\n");
 	printf("Copyright (c) 2001 by David McNab\n\n");
 
-	printf("Usage: fcpput [OPTIONS] key [file]\n\n");
+	printf("Usage: fcpput [OPTIONS] file\n\n");
 
 	printf("Options:\n\n");
 	printf("  -n, --address host     Freenet node address (default \"%s\")\n", EZFCP_DEFAULT_HOST);
 	printf("  -p, --port num         Freenet node port (default %d)\n", EZFCP_DEFAULT_PORT);
 	printf("  -l, --htl num          Hops to live (default %d)\n", EZFCP_DEFAULT_HTL);
 	printf("  -e, --regress num      Number of days to regress (default %d)\n", EZFCP_DEFAULT_REGRESS);
-	printf("  -r, --raw              Raw mode - don't follow redirects\n\n");
-	
-	printf("  -m, --metadata file    Read key's metadata from file (default \"stdin\")\n");
+	printf("  -r, --raw              Raw mode - don't follow redirects\n");
+	printf("  -k, --key key          Name of Freenet key (if unspecified, insert as CHK\n");
+	printf("  -m, --metadata file    Read key's metadata from file (default \"stdin\")\n\n");
+
 	printf("  -v, --verbosity num    Verbosity of log messages (default 2)\n");
 	printf("                         0=silent, 1=critical, 2=normal, 3=verbose, 4=debug\n\n");
 
-	printf("  -q, --quiet            Quiet mode - no prompts for metadata or key data\n");
-  printf("  -a, --attempts num     Attempts to insert each file (default %d)\n", FCPPUT_ATTEMPTS);
+  printf("  -a, --attempts num     Attempts to insert each file (default %d)\n", EZFCP_DEFAULT_INSERTATTEMPTS);
   printf("  -s, --size num         Size of splitfile chunks (default %d)\n", SPLIT_BLOCK_SIZE);
   printf("  -t, --threads num      Number of splitfile threads (default %d)\n\n", FCP_MAX_SPLIT_THREADS);
 
@@ -344,99 +261,3 @@ static void usage(char *s)
 	exit(0);
 }
 
-static char *strsav(char *old, char *text_to_append)
-{
-    int old_len, new_len;
-    char *p;
-
-    if(( text_to_append == 0) || (*text_to_append == '\0')) {
-        return(old);
-    }
-
-    if(old) {
-        old_len = strlen(old);
-    } else {
-        old_len = 0;
-    }
-
-    new_len = old_len + strlen(text_to_append) + 1;
-
-    if(old) {
-        if((p = (char *)realloc(old, new_len)) == 0) {
-//          fprintf(logfp, "%s: realloc(%d) bytes for proxy_args failed!\n", prog, new_len);
-            printf("realloc(%d) bytes for proxy_args failed!\n", new_len);
-            exit(1);
-        }
-    } else {
-        if((p = (char *)safeMalloc(new_len)) == 0) {
-//          fprintf(logfp, "%s: safeMalloc(%d) bytes for proxy_args failed!\n", prog, new_len);
-            printf("safeMalloc(%d) bytes for proxy_args failed!\n", new_len);
-            exit(1);
-        }
-    }
-
-    strcpy(p + old_len, text_to_append);
-    return(p);
-}
-
-
-static char *bufsav(char *old, int old_len, char *buf_to_append, int add_len)
-{
-    int new_len;
-    char *p;
-
-    if(buf_to_append == 0)
-        return(old);
-
-    if(old == 0)
-        old_len = 0;
-
-    new_len = old_len + add_len;
-
-    if(old) {
-        if((p = (char *)realloc(old, new_len)) == 0) {
-//          fprintf(logfp, "%s: realloc(%d) bytes for proxy_args failed!\n", prog, new_len);
-            printf("realloc(%d) bytes for proxy_args failed!\n", new_len);
-            exit(1);
-        }
-    } else {
-        if((p = (char *)safeMalloc(new_len)) == 0) {
-//          fprintf(logfp, "%s: safeMalloc(%d) bytes for proxy_args failed!\n", prog, new_len);
-            printf("safeMalloc(%d) bytes for proxy_args failed!\n", new_len);
-            exit(1);
-        }
-    }
-
-    memcpy(p + old_len, buf_to_append, add_len);
-    return(p);
-}
-
-
-/*
-	Extension of atoi()
-	
-	This func recognises suffices on numbers
-	eg '64k' will get parsed as 65536
-	recognises the suffices 'k', 'K', 'm', 'M', 'g', 'G'
-
-	Thanks to mjr for this lovely snippet
-*/
-
-static int parse_num(char *s)
-{
-	int n = atoi(s);
-	switch (s[strlen(s)-1])
-	{
-	case 'G':
-	case 'g':
-		return n << 30;
-	case 'M':
-	case 'm':
-		return n << 20;
-	case 'K':
-	case 'k':
-		return n << 10;
-	default:
-		return n;
-   }
-}
