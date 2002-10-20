85d85301c4b52d4bdfdb90fca95be91223f31fa1
Gianni Johansson <giannijohansson@attbi.com>
2002-10-20 16:18:41 +0000

    Updated the FEC plug-in implementation to use the 1.0.3 version of the OnionNetworks FEC library. Checked in distribution zip, fec-1.0.3.zip which includes full source and native JNI binaries for Windows and x86 linux.
    There were no changes to the FEC codecs between 1.0.2 and 1.0.3.
    I expect that the new version will be bit-for-bit compatible
    with the previous one.
    
    
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/contrib@1886 67a373e5-eb02-0410-a15c-ee090a768436

--- fecimpl/onion/alien/rebuild.sh
+++ fecimpl/onion/alien/rebuild.sh
@@ -7,12 +7,12 @@ mkdir fec_rebuild
 mkdir fec_rebuild/lib
 cp fec_src/lib/fec.properties fec_rebuild/lib
 
-#javac -classpath .:./common.jar -d fec_rebuild -g -target 1.1 `find fec_src -name '*.java'` 
-javac -classpath .:./common.jar -d fec_rebuild -target 1.1 `find fec_src -name '*.java'` 
-#jikes -classpath .:./common.jar:$JIKESPATH -d fec_rebuild  `find fec_src -name '*.java'` 
+#javac -classpath .:./onion-common.jar -d fec_rebuild -g -target 1.1 `find fec_src -name '*.java'` 
+javac -classpath .:./onion-common.jar -d fec_rebuild -target 1.1 `find fec_src -name '*.java'` 
+#jikes -classpath .:./onion-common.jar:$JIKESPATH -d fec_rebuild  `find fec_src -name '*.java'` 
 cd fec_rebuild
 jar cvf minimal_fec.jar `find -name '*.class'; find -name '*.properties'` 
-mv minimal_fec.jar ../fec.jar
+mv minimal_fec.jar ../onion-fec.jar
 cd ..
 
 rm -rf fec_rebuild
