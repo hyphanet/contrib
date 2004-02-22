af785c12a88be3f99d49088149c52f9b113d24bd
stripwax <stripwax@unknown>
2004-02-22 23:09:53 +0000

    Wonder how long this has been missing from cvs
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/contrib@6125 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ wininstall/Sources/freenet.exe/stdafx.h
@@ -0,0 +1,16 @@
+#ifndef FREENET_STDAFX_H_INCLUDED
+#define FREENET_STDAFX_H_INCLUDED
+
+#define _WIN32_WINVER 0x0500
+#define _WIN32_WINNT 0x0500
+#define WINVER 0x0500
+#include "winsock.h"
+#include "windows.h"
+#include "shellapi.h"
+#include "winnls.h"
+#include "rsrc.h"
+
+#include "types.h"
+#include "shared_data.h"
+
+#endif // FREENET_STDAFX_H_INCLUDED
