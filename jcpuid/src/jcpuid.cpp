b527f50e50a760e4d4d83b5fbf28e0fa6b5ba515
Niklas Bergh <niklas.bergh@tific.com>
2004-07-16 01:31:26 +0000

    A JNI library that can be used to execute the CPUInfo instruction. Currently is contains build information/project files for VS .NET 2003 only.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/contrib@6747 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ jcpuid/src/jcpuid.cpp
@@ -0,0 +1,21 @@
+#include "jcpuid.h"
+
+//Executes the indicated subfunction of the CPUID operation
+JNIEXPORT jobject JNICALL Java_freenet_support_CPUInformation_CPUID_doCPUID
+  (JNIEnv * env, jclass cls, jint iFunction)
+{
+	int a,b,c,d;
+	jclass clsResult = env->FindClass ("freenet/support/CPUInformation/CPUID$CPUIDResult");
+	jmethodID constructor = env->GetMethodID(clsResult,"<init>","(IIII)V" );
+	
+	_asm 
+	{
+		mov eax, iFunction
+		cpuid
+		mov a, eax
+		mov b, ebx
+		mov c, ecx
+		mov d, edx
+	}
+	return env->NewObject(clsResult,constructor,a,b,c,d);
+}
