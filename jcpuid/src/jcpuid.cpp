67f5d78c6717c18bf588de3ccb2b6699e3927ea5
Brandon Low <lostlogic@users.sourceforge.net>
2004-07-21 21:26:30 +0000

    Make it work in MSVC++ and G++
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/contrib@6775 67a373e5-eb02-0410-a15c-ee090a768436

--- jcpuid/src/jcpuid.cpp
+++ jcpuid/src/jcpuid.cpp
@@ -7,6 +7,8 @@ JNIEXPORT jobject JNICALL Java_freenet_support_CPUInformation_CPUID_doCPUID
 	int a,b,c,d;
 	jclass clsResult = env->FindClass ("freenet/support/CPUInformation/CPUID$CPUIDResult");
 	jmethodID constructor = env->GetMethodID(clsResult,"<init>","(IIII)V" );
+	
+#ifndef _MSC_VER
 	asm 
 	(
 		"cpuid"
@@ -16,5 +18,16 @@ JNIEXPORT jobject JNICALL Java_freenet_support_CPUInformation_CPUID_doCPUID
 		  "=d"(d)
 		:"a"(iFunction)
 	);
+#else
+	_asm 
+	{
+		mov eax, iFunction
+		cpuid
+		mov a, eax
+		mov b, ebx
+		mov c, ecx
+		mov d, edx
+	}
+#endif
 	return env->NewObject(clsResult,constructor,a,b,c,d);
 }
