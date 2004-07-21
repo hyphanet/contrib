f2b34e7769b29457fc48ac217ed9709f511046e2
Brandon Low <lostlogic@users.sourceforge.net>
2004-07-21 15:04:29 +0000

    Make compile on gcc
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/contrib@6769 67a373e5-eb02-0410-a15c-ee090a768436

--- jcpuid/src/jcpuid.cpp
+++ jcpuid/src/jcpuid.cpp
@@ -8,14 +8,14 @@ JNIEXPORT jobject JNICALL Java_freenet_support_CPUInformation_CPUID_doCPUID
 	jclass clsResult = env->FindClass ("freenet/support/CPUInformation/CPUID$CPUIDResult");
 	jmethodID constructor = env->GetMethodID(clsResult,"<init>","(IIII)V" );
 	
-	_asm 
-	{
-		mov eax, iFunction
-		cpuid
-		mov a, eax
-		mov b, ebx
-		mov c, ecx
-		mov d, edx
-	}
+	asm 
+	(
+		"mov %eax, iFunction;"
+		"cpuid;"
+		"mov a, %eax;"
+		"mov b, %ebx;"
+		"mov c, %ecx;"
+		"mov d, %edx;"
+	);
 	return env->NewObject(clsResult,constructor,a,b,c,d);
 }
