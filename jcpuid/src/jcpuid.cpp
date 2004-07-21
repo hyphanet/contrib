85bacc180e013bd372afa7ea10c4eb81e2d5ac1d
Niklas Bergh <niklas.bergh@tific.com>
2004-07-21 21:52:31 +0000

    Keep the MSVC code as well as the GCC one
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/contrib@6777 67a373e5-eb02-0410-a15c-ee090a768436

--- jcpuid/src/jcpuid.cpp
+++ jcpuid/src/jcpuid.cpp
@@ -7,27 +7,28 @@ JNIEXPORT jobject JNICALL Java_freenet_support_CPUInformation_CPUID_doCPUID
 	int a,b,c,d;
 	jclass clsResult = env->FindClass ("freenet/support/CPUInformation/CPUID$CPUIDResult");
 	jmethodID constructor = env->GetMethodID(clsResult,"<init>","(IIII)V" );
-	
-#ifndef _MSC_VER
-	asm 
-	(
-		"cpuid"
-		: "=a" (a),
-		  "=b" (b),
-		  "=c"(c),
-		  "=d"(d)
-		:"a"(iFunction)
-	);
-#else
-	_asm 
-	{
-		mov eax, iFunction
-		cpuid
-		mov a, eax
-		mov b, ebx
-		mov c, ecx
-		mov d, edx
-	}
-#endif
+	#ifdef _MSC_VER
+		//Use MSVC assembler notation
+		_asm 
+		{
+			mov eax, iFunction
+			cpuid
+			mov a, eax
+			mov b, ebx
+			mov c, ecx
+			mov d, edx
+		}
+	#else
+		//Use GCC assembler notation
+		asm 
+		(
+			"cpuid"
+			: "=a" (a),
+			  "=b" (b),
+			  "=c"(c),
+			  "=d"(d)
+			:"a"(iFunction)
+		);
+	#endif
 	return env->NewObject(clsResult,constructor,a,b,c,d);
 }
