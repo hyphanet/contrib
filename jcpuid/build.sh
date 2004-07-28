a4fa97064d3a22769f4f1e15ab8dad4a25caa992
Matthew Toseland <toad@amphibian.dyndns.org>
2004-07-28 13:07:53 +0000

    static linking
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/contrib@6831 67a373e5-eb02-0410-a15c-ee090a768436

--- jcpuid/build.sh
+++ jcpuid/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ -shared -Iinclude -I$JAVA_HOME/include -I$JAVA_HOME/include/linux src/*.cpp -o lib/freenet/support/CPUInformation/libjcpuid-x86-linux.so
+g++ -shared -static -static-libgcc -Iinclude -I$JAVA_HOME/include -I$JAVA_HOME/include/linux src/*.cpp -o lib/freenet/support/CPUInformation/libjcpuid-x86-linux.so
