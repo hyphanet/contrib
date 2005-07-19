6b9ae7c7ae052d80fb92c04f4a4febe191213cb4
Conrad J. Sabatier <conrads@cox.net>
2005-07-19 04:03:23 +0000

    Modified and enhanced the build script for libjcpuid, to improve portability, error detection and user feedback.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/contrib@7221 67a373e5-eb02-0410-a15c-ee090a768436

--- jcpuid/build.sh
+++ jcpuid/build.sh
@@ -1,3 +1,54 @@
 #!/bin/sh
+#
+# build.sh -- build an OS/architecture-specific libjcpuid
+#
+# assumes that we're using the GNU C++ compiler, and that our Java includes
+# are relative to the user-defined JAVA_HOME variable (which must be set in
+# the user's environment prior to running this script
+#
 
-g++ -shared -static -static-libgcc -Iinclude -I$JAVA_HOME/include -I$JAVA_HOME/include/linux src/*.cpp -o lib/freenet/support/CPUInformation/libjcpuid-x86-linux.so
+if [ -z "${JAVA_HOME}" ]
+then
+	{	echo
+		echo "Error: JAVA_HOME undefined"
+		echo
+		echo "Type 'export JAVA_HOME=<path to your local Java installation>'"
+		echo "(for example, 'export JAVA_HOME=/usr/local/jdk1.5.0')"
+		echo "and run this script again"
+		echo
+	} >&2
+	exit 1
+fi
+
+# platform-independent variables
+SRC="src/jcpuid.cpp"
+OBJDIR="lib/freenet/support/CPUInformation"
+CXXFLAGS="-shared -static -static-libgcc"
+
+# determine the operating system and machine type
+OS="$(uname -s|tr "[A-Z]" "[a-z]")" 	# convert any uppercase to lowercase
+ARCH=$(uname -m)
+
+# convert machine type of "i386" to "x86"
+[ "${ARCH}" = "i386" ] && ARCH="x86"
+
+# amd64 machine type requires an additional compiler flag to build successfully
+[ "${ARCH}" = "amd64" ] && CXXFLAGS="${CXXFLAGS} -fPIC"
+
+# OK, now we can generate our object file's name and the proper include paths
+OBJ="libjcpuid-${ARCH}-${OS}.so"
+INCLUDES="-Iinclude -I${JAVA_HOME}/include -I${JAVA_HOME}/include/${OS}"
+
+# Finally, do the actual compile
+echo "Building jcpuid library"
+if g++ ${CXXFLAGS} ${INCLUDES} ${SRC} -o ${OBJDIR}/${OBJ}
+then
+	echo "Build successful!"
+	ls -l ${OBJDIR}/${OBJ};
+	exit 0
+else
+	echo "Build failed!" >&2
+	exit 1
+fi
+ 
+
