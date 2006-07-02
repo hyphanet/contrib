33b8a080e62c67bafae8b3197932d9d3905d1928
Florent Daignière <nextgens@freenetproject.org>
2006-07-02 13:25:17 +0000

    freenet-ext: add a version number
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/contrib@9424 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ freenet_ext/ExtVersion.java
@@ -0,0 +1,17 @@
+package freenet.node;
+
+/**
+ * Central spot for stuff related to the versioning of freenet-ext.
+ */
+public abstract class ExtVersion {
+
+	/** The build number of the current revision */
+	private static final int buildNumber = 1;
+
+	public static final int buildNumber() {
+		return buildNumber;
+	}
+	
+	/** Revision number of Version.java as read from CVS */
+	public static final String cvsRevision = "@custom@";
+}
