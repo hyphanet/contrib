d2fb8fb632f6c4695a59c06db7c7cd7186b0cc5f
Florent Daignière <nextgens@freenetproject.org>
2006-08-03 14:50:09 +0000

    new version of freenet-ext.jar : #5
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/contrib@9857 67a373e5-eb02-0410-a15c-ee090a768436

--- freenet_ext/ExtVersion.java
+++ freenet_ext/ExtVersion.java
@@ -6,7 +6,7 @@ package freenet.node;
 public abstract class ExtVersion {
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 2;
+	private static final int buildNumber = 5;
 
 	public static final int buildNumber() {
 		return buildNumber;
