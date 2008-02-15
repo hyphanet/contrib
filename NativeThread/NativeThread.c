a79069ad8092ec833f6454555cbf179c88fbd8c5
Florent Daignière <nextgens@freenetproject.org>
2008-02-15 02:55:13 +0000

    freenet-ext: integration of NativeThread into the build-process, see http://archives.freenetproject.org/thread/20080214.235159.6deed539.en.html
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/contrib@17904 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ NativeThread/NativeThread.c
@@ -0,0 +1,23 @@
+#include<sys/resource.h>
+#include<sys/time.h>
+#include<stdio.h>
+#include <errno.h>
+
+#include"NativeThread.h"
+
+JNIEXPORT jint JNICALL Java_sandbox_NativeThread_getLinuxPriority
+  (JNIEnv * env, jobject jobj) {
+	return getpriority(PRIO_PROCESS, 0);
+}
+
+JNIEXPORT jboolean JNICALL Java_sandbox_NativeThread_setLinuxPriority
+(JNIEnv * env, jobject jobj, jint prio) {
+	int ret;
+	errno = 0;
+	ret = setpriority(PRIO_PROCESS, 0, prio);
+	if (ret == -1 && errno != 0) {
+		printf("Setting the thread priority failed!! %d %d\n",ret,errno);
+		return JNI_FALSE;
+	}
+	return JNI_TRUE;
+}
