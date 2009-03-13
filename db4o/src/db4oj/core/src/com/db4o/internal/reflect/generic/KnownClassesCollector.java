5d6386bdbdb5dd372cbd1ac87596cf53ca60c6bd
Matthew Toseland <toad@amphibian.dyndns.org>
2009-03-13 13:33:21 +0000

    Missing files from db4o update!
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/contrib@26016 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ db4o/src/db4oj/core/src/com/db4o/internal/reflect/generic/KnownClassesCollector.java
@@ -0,0 +1,91 @@
+/* Copyright (C) 2004 - 2008  db4objects Inc.  http://www.db4o.com
+
+This file is part of the db4o open source object database.
+
+db4o is free software; you can redistribute it and/or modify it under
+the terms of version 2 of the GNU General Public License as published
+by the Free Software Foundation and as clarified by db4objects' GPL 
+interpretation policy, available at
+http://www.db4o.com/about/company/legalpolicies/gplinterpretation/
+Alternatively you can write to db4objects, Inc., 1900 S Norfolk Street,
+Suite 350, San Mateo, CA 94403, USA.
+
+db4o is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
+package com.db4o.internal.reflect.generic;
+
+import com.db4o.foundation.*;
+import com.db4o.internal.*;
+import com.db4o.reflect.*;
+import com.db4o.reflect.generic.*;
+
+public class KnownClassesCollector {
+
+	private final ObjectContainerBase _container;
+	private final KnownClassesRepository _repository;
+
+	public KnownClassesCollector(ObjectContainerBase container, KnownClassesRepository repository) {
+		_container = container;
+		_repository = repository;
+	}
+
+	public ReflectClass[] collect() {
+        Collection4 classes = new Collection4();
+		collectKnownClasses(classes);
+		
+		return (ReflectClass[])classes.toArray(new ReflectClass[classes.size()]);
+	}
+	
+	private void collectKnownClasses(final Collection4 classes) {
+		final Listener collectingListener = newCollectingClassListener(classes);
+		_repository.addListener(collectingListener);
+		try { 
+			collectKnownClasses(classes, Iterators.copy(_repository.classes()));
+		} finally { 
+			_repository.removeListener(collectingListener);
+		}
+	}
+
+	private Listener newCollectingClassListener(final Collection4 classes) {
+		return new Listener() {		
+			public void onEvent(Object addedClass) {
+				collectKnownClass(classes, (ReflectClass) addedClass);
+			}
+		};
+	}
+
+	private void collectKnownClasses(Collection4 collector, Iterator4 knownClasses) {
+		while(knownClasses.moveNext()){
+            ReflectClass clazz = (ReflectClass) knownClasses.current();
+            collectKnownClass(collector, clazz);
+		}
+	}
+
+	private void collectKnownClass(Collection4 classes, ReflectClass clazz) {
+		if(isInternalClass(clazz))
+			return;
+		
+		if(isSecondClass(clazz))
+			return;
+		
+		if(clazz.isArray())
+			return;
+		
+		classes.add(clazz);
+	}
+
+	private boolean isInternalClass(ReflectClass clazz) {
+		return _container._handlers.ICLASS_INTERNAL.isAssignableFrom(clazz);
+	}
+
+	private boolean isSecondClass(ReflectClass clazz) {
+		ClassMetadata clazzMeta = _container.classMetadataForReflectClass(clazz);
+		return clazzMeta != null && clazzMeta.isSecondClass();
+	}
+}
---
+++ db4o/src/db4ojdk1.2/test/src/com/db4o/db4ounit/common/migration/KnownClassesMigrationTestCase.java
@@ -0,0 +1,72 @@
+/* Copyright (C) 2004 - 2008  db4objects Inc.  http://www.db4o.com
+
+This file is part of the db4o open source object database.
+
+db4o is free software; you can redistribute it and/or modify it under
+the terms of version 2 of the GNU General Public License as published
+by the Free Software Foundation and as clarified by db4objects' GPL 
+interpretation policy, available at
+http://www.db4o.com/about/company/legalpolicies/gplinterpretation/
+Alternatively you can write to db4objects, Inc., 1900 S Norfolk Street,
+Suite 350, San Mateo, CA 94403, USA.
+
+db4o is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
+package com.db4o.db4ounit.common.migration;
+
+import java.util.*;
+
+import com.db4o.config.*;
+import com.db4o.db4ounit.common.handlers.*;
+import com.db4o.ext.*;
+import com.db4o.foundation.*;
+import com.db4o.reflect.*;
+
+import db4ounit.*;
+
+/**
+ * @sharpen.ignore
+ * @decaf.ignore.jdk11
+ */
+public class KnownClassesMigrationTestCase extends FormatMigrationTestCaseBase {
+
+	protected void assertObjectsAreReadable(ExtObjectContainer objectContainer) {
+		if (isVersionWithoutTCollection())
+			return;
+		
+		final ReflectClass[] knownClasses = objectContainer.knownClasses();
+		
+		Assert.isNotNull(knownClasses);
+		Assert.isGreater(2, knownClasses.length);
+		
+		ReflectClass type = objectContainer.reflector().forClass(TCollection.class);
+		Assert.isGreaterOrEqual(0, Arrays4.indexOf(knownClasses, type));
+	}
+
+	private boolean isVersionWithoutTCollection() {
+		return db4oMajorVersion() < 5;
+	}
+
+	protected String fileNamePrefix() {
+		return "KnownClasses";
+	}
+
+	protected void store(ExtObjectContainer objectContainer) {
+		objectContainer.set(new Item(new LinkedList()));
+	}
+	
+	private static class Item {
+		public Item(LinkedList list) {
+			_list = list;
+		}
+		
+		public LinkedList _list;
+	}
+
+}
