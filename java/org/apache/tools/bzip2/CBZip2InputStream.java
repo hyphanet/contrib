fa4e08f7cb57e656c8deb38df5d8763e7c40fae2
Daniel Cheng (鄭郁邦) <j16sdiz@freenetproject.org>
2009-06-09 20:43:51 +0800

    Apache BZip2 switch to ANT_171
    This match what we have been using in ext#26
    
    @http://svn.apache.org/repos/asf/ant/core/tags/ANT_171/src/main/org/apache/tools/bzip2/

--- java/org/apache/tools/bzip2/CBZip2InputStream.java
+++ java/org/apache/tools/bzip2/CBZip2InputStream.java
@@ -23,9 +23,8 @@
  */
 package org.apache.tools.bzip2;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
+import java.io.IOException;
 
 /**
  * An input stream that decompresses from the BZip2 format (without the file
@@ -224,12 +223,6 @@ public class CBZip2InputStream extends InputStream implements BZip2Constants {
     }
 
     private void init() throws IOException {
-        if (null == in) {
-            throw new IOException("No InputStream");
-        }
-        if (in.available() == 0) {
-            throw new IOException("Empty InputStream");
-        }
         int magic2 = this.in.read();
         if (magic2 != 'h') {
             throw new IOException("Stream is not BZip2 formatted: expected 'h'"
