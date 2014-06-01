1fc0532a12d42472c766b1687c443cfae13350fc
Abominatio <Abominatio@users.noreply.github.com>
2014-05-31 21:52:23 -0400

    Coding Standards Audit: Part 7
    Verified all code meets the “Freenet Java Coding Standards”. –
    (https://wiki.freenetproject.org/Coding_standards)

--- onion-fec/src/com/onionnetworks/fec/Native16Code.java
+++ onion-fec/src/com/onionnetworks/fec/Native16Code.java
@@ -1,5 +1,7 @@
 package com.onionnetworks.fec;
 
+//~--- non-JDK imports --------------------------------------------------------
+
 //import java.security.AccessController;
 //import sun.security.action.*;
 import com.onionnetworks.util.*;
@@ -14,57 +16,52 @@ import com.onionnetworks.util.*;
  * @author Justin F. Chapweske (justin@chapweske.com)
  */
 public class Native16Code extends FECCode {
+    static {
+        String path = NativeDeployer.getLibraryPath(Native8Code.class.getClassLoader(), "fec16");
+
+        if (path != null) {
+            System.load(path);
+            initFEC();
+        } else {
+            System.out.println("Unable to find native library for fec16 for platform " + NativeDeployer.OS_ARCH);
+            System.out.println(path);
+        }
+    }
 
     // One must be very very careful not to let code escape, it stores the
     // memory address of a fec_parms struct and if modified could give an
     // attacker the ability to point to anything in memory.
     final private long code;
 
-    static {
-        String path = NativeDeployer.getLibraryPath
-            (Native8Code.class.getClassLoader(),"fec16");
-        if (path != null) {
-            System.load(path);
-            initFEC();
-        } else {
-            System.out.println("Unable to find native library for fec16 for platform "+NativeDeployer.OS_ARCH);
-            System.out.println(path);
-        }
-    }
-
     public Native16Code(int k, int n) {
-        super(k,n);
-        code = nativeNewFEC(k,n);
+        super(k, n);
+        code = nativeNewFEC(k, n);
     }
 
-    protected void encode(byte[][] src, int[] srcOff, byte[][] repair,
-                          int[] repairOff, int[] index, int packetLength) {
-
+    protected void encode(byte[][] src, int[] srcOff, byte[][] repair, int[] repairOff, int[] index, int packetLength) {
         if (packetLength % 2 != 0) {
-            throw new IllegalArgumentException("For 16 bit codes, buffers "+
-                                               "must be 16 bit aligned.");
+            throw new IllegalArgumentException("For 16 bit codes, buffers " + "must be 16 bit aligned.");
         }
-        nativeEncode(src,srcOff,index,repair,repairOff,k,packetLength);
+
+        nativeEncode(src, srcOff, index, repair, repairOff, k, packetLength);
     }
 
-    protected void decode(byte[][] pkts, int[] pktsOff,
-                          int[] index, int packetLength, boolean inOrder) {
+    protected void decode(byte[][] pkts, int[] pktsOff, int[] index, int packetLength, boolean inOrder) {
         if (packetLength % 2 != 0) {
-            throw new IllegalArgumentException("For 16 bit codes, buffers "+
-                                               "must be 16 bit aligned.");
+            throw new IllegalArgumentException("For 16 bit codes, buffers " + "must be 16 bit aligned.");
         }
+
         if (!inOrder) {
-            shuffle(pkts,pktsOff,index,k);
+            shuffle(pkts, pktsOff, index, k);
         }
-        nativeDecode(pkts,pktsOff,index,k,packetLength);
+
+        nativeDecode(pkts, pktsOff, index, k, packetLength);
     }
 
-    protected native void nativeEncode
-        (byte[][] src, int[] srcOff, int[] index, byte[][] repair,
-         int[] repairOff, int k, int packetLength);
+    protected native void nativeEncode(byte[][] src, int[] srcOff, int[] index, byte[][] repair, int[] repairOff,
+                                       int k, int packetLength);
 
-    protected native void nativeDecode(byte[][] pkts, int[] pktsOff,
-                                       int[] index, int k, int packetLength);
+    protected native void nativeDecode(byte[][] pkts, int[] pktsOff, int[] index, int k, int packetLength);
 
     protected synchronized native long nativeNewFEC(int k, int n);
 
@@ -77,6 +74,6 @@ public class Native16Code extends FECCode {
     }
 
     public String toString() {
-        return new String("Native16Code[k="+k+",n="+n+"]");
+        return new String("Native16Code[k=" + k + ",n=" + n + "]");
     }
 }
--- onion-fec/src/com/onionnetworks/fec/Pure16Code.java
+++ onion-fec/src/com/onionnetworks/fec/Pure16Code.java
@@ -1,7 +1,10 @@
 package com.onionnetworks.fec;
 
-import com.onionnetworks.util.Util;
+//~--- non-JDK imports --------------------------------------------------------
+
 import com.onionnetworks.util.Buffer;
+import com.onionnetworks.util.Util;
+
 /**
  * This class, along with FECMath, provides the implementation of the pure
  * Java 16 bit FEC codes.  This is heavily dervied from Luigi Rizzos original
@@ -14,21 +17,19 @@ import com.onionnetworks.util.Buffer;
  * @author Justin F. Chapweske (justin@chapweske.com)
  */
 public class Pure16Code extends PureCode {
-
     protected static final FECMath fecMath = new FECMath(16);
 
-
-    /** Notes about large N support:
-        you can just generate the top k*k vandermonde matrix, call it V,
-        then invert it, then when you have k blocks generate a matrix M
-        with the k rows you need <r_i> and E = M* V^{-1} is the encoding matrix
-        for the systematic code which you then need to invert to perform
-        the decoding. Probably there is a fast way to invert E given that M
-        is also a vandermonde matrix so it is "easy" to compute M^{-1}
-    */
-
+    /**
+     * Notes about large N support:
+     *   you can just generate the top k*k vandermonde matrix, call it V,
+     *   then invert it, then when you have k blocks generate a matrix M
+     *   with the k rows you need <r_i> and E = M* V^{-1} is the encoding matrix
+     *   for the systematic code which you then need to invert to perform
+     *   the decoding. Probably there is a fast way to invert E given that M
+     *   is also a vandermonde matrix so it is "easy" to compute M^{-1}
+     */
     public Pure16Code(int k, int n) {
-        super(k,n,fecMath.createEncodeMatrix(k,n));
+        super(k, n, fecMath.createEncodeMatrix(k, n));
     }
 
     /**
@@ -36,30 +37,28 @@ public class Pure16Code extends PureCode {
      * and produces as output a packet pointed to by fec, computed
      * with index "index".
      */
-    protected void encode(byte[][] src, int[] srcOff, byte[][] repair, 
-                          int[] repairOff, int[] index, int packetLength) {
+    protected void encode(byte[][] src, int[] srcOff, byte[][] repair, int[] repairOff, int[] index, int packetLength) {
         if (packetLength % 2 != 0) {
-            throw new IllegalArgumentException("For 16 bit codes, buffers "+
-                                               "must be 16 bit aligned.");
+            throw new IllegalArgumentException("For 16 bit codes, buffers " + "must be 16 bit aligned.");
         }
+
         char[][] srcChars = new char[src.length][];
         int[] srcCharsOff = new int[src.length];
-        int numChars = packetLength/2;
+        int numChars = packetLength / 2;
         char[] repairChars = new char[numChars];
-        for (int i=0;i<srcChars.length;i++) {
+
+        for (int i = 0; i < srcChars.length; i++) {
             srcChars[i] = new char[numChars];
             Util.arraycopy(src[i], srcOff[i], srcChars[i], 0, packetLength);
             srcCharsOff[i] = 0;
         }
 
-        for (int i=0;i<repair.length;i++) {
-            if (index[i] < k) { // < k, systematic so direct copy.
-                System.arraycopy(src[index[i]],srcOff[index[i]],repair[i],
-                                 repairOff[i], packetLength);
+        for (int i = 0; i < repair.length; i++) {
+            if (index[i] < k) {    // < k, systematic so direct copy.
+                System.arraycopy(src[index[i]], srcOff[index[i]], repair[i], repairOff[i], packetLength);
             } else {
-                encode(srcChars,srcCharsOff,repairChars,0,index[i],numChars);
-                Util.arraycopy(repairChars,0,repair[i],repairOff[i],
-                               packetLength);
+                encode(srcChars, srcCharsOff, repairChars, 0, index[i], numChars);
+                Util.arraycopy(repairChars, 0, repair[i], repairOff[i], packetLength);
             }
         }
     }
@@ -67,21 +66,19 @@ public class Pure16Code extends PureCode {
     /**
      * ASSERT: index >= k && index < n
      */
-    protected void encode(char[][] src, int[] srcOff, char[] repair, 
-                          int repairOff, int index, int numChars) {
-        int pos = index*k;
-        Util.bzero(repair,repairOff,numChars);
-        for (int i=0; i<k ; i++) {
-            fecMath.addMul(repair,repairOff,src[i],srcOff[i],
-                           encMatrix[pos+i],numChars);
+    protected void encode(char[][] src, int[] srcOff, char[] repair, int repairOff, int index, int numChars) {
+        int pos = index * k;
+
+        Util.bzero(repair, repairOff, numChars);
+
+        for (int i = 0; i < k; i++) {
+            fecMath.addMul(repair, repairOff, src[i], srcOff[i], encMatrix[pos + i], numChars);
         }
     }
-    
-    protected void decode(byte[][] pkts, int[] pktsOff, int[] index, 
-                          int packetLength, boolean inOrder) {          
+
+    protected void decode(byte[][] pkts, int[] pktsOff, int[] index, int packetLength, boolean inOrder) {
         if (packetLength % 2 != 0) {
-            throw new IllegalArgumentException("For 16 bit codes, buffers "+
-                                               "must be 16 bit aligned.");
+            throw new IllegalArgumentException("For 16 bit codes, buffers " + "must be 16 bit aligned.");
         }
 
         if (!inOrder) {
@@ -90,8 +87,9 @@ public class Pure16Code extends PureCode {
 
         char[][] pktsChars = new char[pkts.length][];
         int[] pktsCharsOff = new int[pkts.length];
-        int numChars = packetLength/2;
-        for (int i=0;i<pktsChars.length;i++) {
+        int numChars = packetLength / 2;
+
+        for (int i = 0; i < pktsChars.length; i++) {
             pktsChars[i] = new char[numChars];
             Util.arraycopy(pkts[i], pktsOff[i], pktsChars[i], 0, packetLength);
             pktsCharsOff[i] = 0;
@@ -99,36 +97,34 @@ public class Pure16Code extends PureCode {
 
         char[][] result = decode(pktsChars, pktsCharsOff, index, numChars);
 
-        for (int i=0;i<result.length;i++) {
+        for (int i = 0; i < result.length; i++) {
             if (result[i] != null) {
-                Util.arraycopy(result[i], 0, pkts[i], pktsOff[i], 
-                               packetLength);
+                Util.arraycopy(result[i], 0, pkts[i], pktsOff[i], packetLength);
                 index[i] = i;
             }
         }
     }
 
-    protected char[][] decode(char[][] pkts, int[] pktsOff, int[] index, 
-                          int numChars) {
+    protected char[][] decode(char[][] pkts, int[] pktsOff, int[] index, int numChars) {
+        char[] decMatrix = fecMath.createDecodeMatrix(encMatrix, index, k, n);
 
-        char[] decMatrix = fecMath.createDecodeMatrix(encMatrix,index,k,n);
-        
         // do the actual decoding
         char[][] tmpPkts = new char[k][];
-        for (int row=0; row<k; row++) {
+
+        for (int row = 0; row < k; row++) {
             if (index[row] >= k) {
                 tmpPkts[row] = new char[numChars];
-                for (int col=0 ; col<k ; col++) {
-                    fecMath.addMul(tmpPkts[row],0,pkts[col],pktsOff[col], 
-                                   decMatrix[row*k + col], numChars);
+
+                for (int col = 0; col < k; col++) {
+                    fecMath.addMul(tmpPkts[row], 0, pkts[col], pktsOff[col], decMatrix[row * k + col], numChars);
                 }
             }
         }
 
         return tmpPkts;
     }
-    
+
     public String toString() {
-        return new String("Pure16Code[k="+k+",n="+n+"]");
+        return new String("Pure16Code[k=" + k + ",n=" + n + "]");
     }
 }
